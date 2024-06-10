0bcad5932db9d61b1d3c23197088b4c02438d0cc
Vadim <vadimx30@gmail.com>
2024-06-10 12:24:06 +0300

    Initial commit

---
+++ go_mongodb/start/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	// "go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+)
+
+func main() {
+	ctx := context.TODO()
+	opts := options.Client().ApplyURI("mongodb://localhost:27017")
+
+	// Get client to work to mongo server
+	client, err := mongo.Connect(ctx, opts)
+	if err != nil {
+		log.Fatal(err)
+	}
+	
+	// Close connection
+	defer client.Disconnect(ctx)
+
+	fmt.Printf("%T\n", client)
+
+	if err := client.Ping(ctx, readpref.Primary()); err != nil {
+		log.Fatal(err)
+	}
+}
