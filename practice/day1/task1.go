ce6062377037be782b18cd936d4a3ebcd445a77c
Vadim <vadimx30@gmail.com>
2024-06-10 15:00:22 +0300

    Practice task1

---
+++ practice/day1/task1.go
@@ -0,0 +1,15 @@
+package main
+
+
+// TODO. Реализовать CRUD для коллекции "Книги"
+
+type Book struct{
+	Title		string
+	Author		string
+	ISBN		string
+	Publisher	string
+	Year		int
+	Copies		int
+}
+
+func main(){}
